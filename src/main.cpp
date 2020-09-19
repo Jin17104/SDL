51972c9b98322441c03f3201977997733e08a18d
Jin17104 <64642695+Jin17104@users.noreply.github.com>
2020-09-19 01:38:20 -0700

    Added Window
    1st Step Completed Added Window Mode

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,38 @@
 #include <SDL2/SDL_image.h>
 #include <iostream>
 
+#include "RenderWindow.hpp"
+
 int main(int argc, char* args[])
 {
-	std::cout << "Hello World" << std::endl;
-	std::cin.get();
+	if(SDL_Init(SDL_INIT_VIDEO) > 0)
+	{
+		std::cout << "Hey.. SDL_Init has Failed. SDL_ERROR:  " << SDL_GetError() << std::endl; 
+	}
+
+	if(!(IMG_Init(IMG_INIT_PNG)))
+	{
+		std::cout << "IMG_Imit has failed. Error: " << SDL_GetError() << std::endl; 
+	}
+
+	RenderWindow window("Game v1", 1280, 720);
+
+	bool gameRunning = true; 
+	SDL_Event event; 
+	
+	while(gameRunning)
+	{
+		while(SDL_PollEvent(&event))
+		{
+			if(event.type == SDL_QUIT)
+			{
+				gameRunning = false; 
+			}
+		}
+	}
+
+	window.cleanUp(); 
+	SDL_Quit(); 
 
 	return 0; 
 }
